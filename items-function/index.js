d685c305ddc93d97cc7a5ac77c4c6b35a5f939a5
Vaclav Malinak <vaclavm@kentico.com>
2019-01-04 14:45:32 +0100

    Add express to Azure Function

--- items-function/index.js
+++ items-function/index.js
@@ -1,6 +1,22 @@
-module.exports = async (context, request) => {
-    context.res = {
-        status: 200,
-        body: 'Published successfully'
-    }
-};
+const createHandler = require("azure-function-express").createHandler;
+const express = require("express");
+ 
+// Create express app as usual
+const app = express();
+
+app.get("/api/items/:id", (req, res) => {
+    res.json({
+        type: 'GET_ONE',
+        id: req.params.id
+    });
+});
+
+
+app.get("/api/items", (req, res) => {
+    res.json({
+        type: 'GET_ALL',
+    });
+});
+ 
+// Binds the express app to an Azure Function handler
+module.exports = createHandler(app);
